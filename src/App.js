bbf580e437c8901d166a5b5fc05c2a2fb6b67bf2
Nathan Hietala <Nrhietala@gmail.com>
2021-07-12 23:08:23 -0500

    Adds starter routing and index for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { StyleReset } from "./styles/index";
+import { Route, Switch } from "react-router-dom";
+import { StyleReset } from "./styles";
+import { Home } from "./components/pages";
 
 function App() {
   return (
     <>
       <StyleReset />
       <p>I'm working 🙂</p>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 }

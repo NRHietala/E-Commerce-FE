442133a2f8eade0d2407e1b9f34063f269936da6
Nathan Hietala <Nrhietala@gmail.com>
2021-07-13 23:23:04 -0500

    Adds basic redux infrastructure

---
+++ src/redux/productSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const weatherSlice = createSlice({
+  name: "products",
+  initialState: [],
+  reducers: {},
+  extraReducers: {},
+});
+
+// export extraReducer actions below
+
+// export const { } = weatherSlice.actions;
+
+export default weatherSlice.reducer;
